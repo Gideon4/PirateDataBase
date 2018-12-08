27b10e17b789098db2fe50164959e624ef0679b0
Gideon4 <43590645+Gideon4@users.noreply.github.com>
2018-12-08 11:42:47 -0600

    arrow scroll

---
+++ PirateDataBase-master/FirebaseManager.py
@@ -0,0 +1,11 @@
+from firebase import firebase as fb
+
+class FirebaseManager:
+    app = fb.FirebaseApplication("https://fihreebahsay.firebaseio.com/")
+
+    def writetofile(self, idNum, obj):
+        result = self.app.put("", idNum, obj)
+
+    def getall(self):
+        d = self.app.get("",None)
+        return d
