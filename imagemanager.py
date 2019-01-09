df809987daf6d0a2b4c86c861c20a81a75d0860e
Gideon4 <43590645+Gideon4@users.noreply.github.com>
2019-01-08 19:38:33 -0600

    Add files via upload

--- imagemanager.py
+++ imagemanager.py
@@ -1,11 +1,19 @@
 from google.cloud import storage
 import os
+import datetime
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
+class ImageManager:
 
-client = storage.Client()
-bucket = client.get_bucket("fihreebahsay.appspot.com")
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 
-imagepath = "pirate1.gif"
-imageBlob = bucket.blob("images/" + imagepath)
-imageBlob.upload_from_filename(imagepath)
+    client = storage.Client()
+    bucket = client.get_bucket("fihreebahsay.appspot.com")
+    url = ""
+    imagepath = "pirate1.gif"
+
+    def uploadImage(self):
+        imageBlob = self.bucket.blob("images/" + imagepath)
+        imageBlob.upload_from_filename(imagepath)
+        d = datetime.datetime(3005,12,5)
+        self.url = imageBlob.generate_signed_url(d)
+
